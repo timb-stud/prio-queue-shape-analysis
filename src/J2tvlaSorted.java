d0f0aaa8846818ac3035a7fa4ff0e32693e78c37
Tim Bartsch <tibartschm@googlemail.com>
2012-07-27 10:33:41 +0200

    Added a class for each PriorityQueue, that calls every function of that PriorityQueue.
    We need that for running j2tvla..

---
+++ src/J2tvlaSorted.java
@@ -0,0 +1,12 @@
+import de.htw.PriorityQueueSorted;
+
+
+public class J2tvlaSorted {
+	public static void main(String[] args) {
+		PriorityQueueSorted pq = new PriorityQueueSorted();
+		pq.enqueue(1234, 1);
+		pq.isEmpty();
+		pq.peek();
+		pq.dequeue();
+	}
+}
---
+++ src/J2tvlaUnsorted.java
@@ -0,0 +1,12 @@
+import de.htw.PriorityQueueUnsorted;
+
+
+public class J2tvlaUnsorted {
+	public static void main(String[] args) {
+		PriorityQueueUnsorted pq = new PriorityQueueUnsorted();
+		pq.enqueue(1234, 1);
+		pq.isEmpty();
+		pq.peek();
+		pq.dequeue();
+	}
+}
