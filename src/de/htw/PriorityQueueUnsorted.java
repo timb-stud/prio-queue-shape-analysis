d8a0c3a56fe3e2ae87f99a1e3e8cf197429beafa
Tim Bartsch <tibartschm@googlemail.com>
2012-08-07 17:24:59 +0200

    Using data instead of newElement.data

--- src/de/htw/PriorityQueueUnsorted.java
+++ src/de/htw/PriorityQueueUnsorted.java
@@ -96,7 +96,7 @@ public class PriorityQueueUnsorted implements IPriorityQueue {
 		//if list contains element with the same data, the priority must be updated
 		QueueElement current = this.head;
 		while (current != null){
-			if (current.data == newElement.data){
+			if (current.data == data){
 				current.priority = newElement.priority;
 				return false;
 			}
