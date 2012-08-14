b978ffa81940c4010b0830458191fef26af4240f
Tim Bartsch <tibartschm@googlemail.com>
2012-08-14 17:12:56 +0200

    Added skip and exit_false to l68

--- src/de/htw/PriorityQueueSorted.java
+++ src/de/htw/PriorityQueueSorted.java
@@ -65,7 +65,7 @@ public class PriorityQueueSorted implements IPriorityQueue {
 
 		// Override head data
 		if (head.data == data) { //Add data_equals action
-			head.priority = priority; //Add Set_priority action; goto return false; update set_priority action
+			head.priority = priority; //Add Set_priority action; update set_priority action
 			return false;
 		}
 
