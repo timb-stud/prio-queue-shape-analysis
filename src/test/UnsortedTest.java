75afd52c1cc37953cff08b0d86cdc9e799f7eae9
rechc <rech.christian@googlemail.com>
2012-09-06 12:13:19 +0200

    renamed some unit methods

--- src/test/UnsortedTest.java
+++ src/test/UnsortedTest.java
@@ -11,7 +11,7 @@ public class UnsortedTest extends Assert {
 	
 
 	@Test
-	public void elements1() {
+	public void enqueueElm1() {
 		PriorityQueueUnsorted pq = new PriorityQueueUnsorted();
 		QueueElement qe1 = new QueueElement(1, 5);
 		QueueElement qe2 = new QueueElement(12, 10);
@@ -34,7 +34,7 @@ public class UnsortedTest extends Assert {
 	}
 
 	@Test
-	public void elements2() {
+	public void enqeueElm2() {
 		PriorityQueueUnsorted pq = new PriorityQueueUnsorted();
 		QueueElement qe1 = new QueueElement(1, 5);
 		QueueElement qe2 = new QueueElement(3, 10);
@@ -54,7 +54,48 @@ public class UnsortedTest extends Assert {
 	}
 	
 	@Test
-	public void elements2update() {
+	public void enqeueElm3() {
+		PriorityQueueUnsorted pq = new PriorityQueueUnsorted();
+		QueueElement qe1 = new QueueElement(1, 5);
+		QueueElement qe2 = new QueueElement(12, 10);
+		QueueElement qe3 = new QueueElement(123, 15);
+		QueueElement qe4 = new QueueElement(1234, 1);
+		QueueElement qe5 = new QueueElement(12345, 6);
+		
+		qe5.next = qe4;
+		qe4.next = qe3;
+		qe3.next = qe2;
+		qe2.next = qe1;
+		
+		assertTrue(pq.enqueue(qe1.data, qe1.priority));
+		assertTrue(pq.enqueue(qe2.data, qe2.priority));
+		assertTrue(pq.enqueue(qe3.data, qe3.priority));
+		assertTrue(pq.enqueue(qe4.data, qe4.priority));
+		assertTrue(pq.enqueue(qe5.data, qe5.priority));
+		assertListEquals(qe5, pq.head);
+	}
+
+	@Test
+	public void enqeueElm4() {
+		PriorityQueueUnsorted pq = new PriorityQueueUnsorted();
+		QueueElement qe1 = new QueueElement(1, 5);
+		QueueElement qe2 = new QueueElement(3, 10);
+		QueueElement qe3 = new QueueElement(99, 15);
+		QueueElement qe4 = new QueueElement(123, 1);
+		
+		qe4.next = qe3;
+		qe3.next = qe2;
+		qe2.next = qe1;
+		
+		assertTrue(pq.enqueue(qe1.data, qe1.priority));
+		assertTrue(pq.enqueue(qe2.data, qe2.priority));
+		assertTrue(pq.enqueue(qe3.data, qe3.priority));
+		assertTrue(pq.enqueue(qe4.data, qe4.priority));
+		assertListEquals(qe4, pq.head);
+	}
+	
+	@Test
+	public void enqeueElm2update() {
 		PriorityQueueUnsorted pq = new PriorityQueueUnsorted();
 		QueueElement qe1 = new QueueElement(1, 5);
 		QueueElement qe2 = new QueueElement(3, 10);
@@ -76,48 +117,7 @@ public class UnsortedTest extends Assert {
 
 
 	@Test
-	public void enqueueElements1() {
-		PriorityQueueUnsorted pq = new PriorityQueueUnsorted();
-		QueueElement qe1 = new QueueElement(1, 5);
-		QueueElement qe2 = new QueueElement(12, 10);
-		QueueElement qe3 = new QueueElement(123, 15);
-		QueueElement qe4 = new QueueElement(1234, 1);
-		QueueElement qe5 = new QueueElement(12345, 6);
-		
-		qe5.next = qe4;
-		qe4.next = qe3;
-		qe3.next = qe2;
-		qe2.next = qe1;
-		
-		assertTrue(pq.enqueue(qe1.data, qe1.priority));
-		assertTrue(pq.enqueue(qe2.data, qe2.priority));
-		assertTrue(pq.enqueue(qe3.data, qe3.priority));
-		assertTrue(pq.enqueue(qe4.data, qe4.priority));
-		assertTrue(pq.enqueue(qe5.data, qe5.priority));
-		assertListEquals(qe5, pq.head);
-	}
-
-	@Test
-	public void enqueueElements2() {
-		PriorityQueueUnsorted pq = new PriorityQueueUnsorted();
-		QueueElement qe1 = new QueueElement(1, 5);
-		QueueElement qe2 = new QueueElement(3, 10);
-		QueueElement qe3 = new QueueElement(99, 15);
-		QueueElement qe4 = new QueueElement(123, 1);
-		
-		qe4.next = qe3;
-		qe3.next = qe2;
-		qe2.next = qe1;
-		
-		assertTrue(pq.enqueue(qe1.data, qe1.priority));
-		assertTrue(pq.enqueue(qe2.data, qe2.priority));
-		assertTrue(pq.enqueue(qe3.data, qe3.priority));
-		assertTrue(pq.enqueue(qe4.data, qe4.priority));
-		assertListEquals(qe4, pq.head);
-	}
-	
-	@Test
-	public void enqueueElements2update() {
+	public void enqeueElm3update() {
 		PriorityQueueUnsorted pq = new PriorityQueueUnsorted();
 		QueueElement qe1 = new QueueElement(1, 5);
 		QueueElement qe2 = new QueueElement(3, 10);
@@ -141,7 +141,7 @@ public class UnsortedTest extends Assert {
 	}
 	
 	@Test
-	public void addOneElement_updatePriority() {
+	public void enqueueOneElement_updatePriority() {
 		PriorityQueueUnsorted pq = new PriorityQueueUnsorted();
 		QueueElement qe1 = new QueueElement(1, 1);
 		QueueElement qe2 = new QueueElement(1, 2);
@@ -173,5 +173,6 @@ public class UnsortedTest extends Assert {
 		}
 		return sb.toString();
 	}
+	
 
 }
